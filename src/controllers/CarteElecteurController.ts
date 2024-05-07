ebfc55a40a3b9c43a8e5b4aa07a435f2d0217825
Samrou Teouri <teourisamrou@gmail.com>
2024-05-07 13:27:43 +0000

    CarteElecteurService : store

--- src/controllers/CarteElecteurController.ts
+++ src/controllers/CarteElecteurController.ts
@@ -1,51 +1,14 @@
 import { Request, Response } from "express";
-import { User } from "../models/User";
-import { Citoyen } from "../models/Citoyen";
-import { CarteElecteur } from "../models/CarteElecteur";
-import { AgentElectoral } from "../models/AgentElectoral";
+import { CarteElecteurService } from "../services/CarteElecteurService";
 
-export const createElectoralCard = async (req: Request, res: Response) => {
-    const user : User = req.body.user;
+const carteElecteurService = new CarteElecteurService();
 
-    const { nom, prenom, dateNaissance, lieuNaissance, profession, nom_mere, nom_pere } = req.body;
-    
+export const generateElectoralCard = async (req: Request, res: Response) => {
     try {
-        await user.getCitoyen()
-            .then(async (citoyen: Citoyen) => {
-                await citoyen.update({
-                    nom: nom,
-                    prenom: prenom,
-                    dateNaissance: dateNaissance,
-                    lieuNaissance: lieuNaissance,
-                    profession: profession,
-                    nom_pere: nom_pere,
-                    nom_mere: nom_mere,
-                }).catch((reason: any) => {
-                    throw reason;
-                });
-
-                await citoyen.createCarteElecteur()
-                    .then(async (carteElecteur: CarteElecteur) => {
-                        carteElecteur.update({
-                            numero: carteElecteur.createNumber(),
-                        }).catch((reason: any) => {
-                            throw reason;
-                        });
-
-                        (await user.getCitoyen()).getAgentElectoral()
-                            .then(async (agentElectoral: AgentElectoral) => {
-                                await carteElecteur.setAgentElectoral(agentElectoral);
-                            }).catch((reason: any) => {
-                                throw reason;
-                            });
-                    })
-                    .catch((reason: any) => {
-                        throw reason;
-                    });
-            }).catch((reason: any) => {
-                throw reason;
-            });
+        const carteElecteur = await carteElecteurService.store(req);
+        if (carteElecteur) return res.status(201).json({ message: "Carte électorale générée avec succès"});
     } catch (error) {
         console.error(error);
+        return res.status(500).json({ message: "Erreur lors de la génération de la carte électorale"});
     }
 }
